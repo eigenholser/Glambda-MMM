9fc83957435ea7f3d50818e3b937bd7ebdf5d094
Scott Overholser <eigenholser@users.noreply.github.com>
2020-10-24 21:40:57 -0600

    Change field for clarity.

--- src/main/java/com/zyx2/glambda/mmm/ApiResult.java
+++ src/main/java/com/zyx2/glambda/mmm/ApiResult.java
@@ -2,27 +2,27 @@ package com.zyx2.glambda.mmm;
 
 public class ApiResult {
 	private MeanMedianMode result;
-	private String status;
+	private String message;
 
 	public ApiResult() {
 
 	}
 
-	public ApiResult(String status, MeanMedianMode result) {
+	public ApiResult(String message, MeanMedianMode result) {
 		this.result = result;
-		this.status = status;
+		this.message = message;
 	}
 
 	public MeanMedianMode getResult() {
 		return result;
 	}
 
-	public String getStatus() {
-		return status;
+	public String getMessage() {
+		return message;
 	}
 
 	@Override
 	public String toString() {
-		return "ApiResult [result=" + result + ", status=" + status + "]";
+		return "ApiResult [result=" + result + ", message=" + message + "]";
 	}
 }
